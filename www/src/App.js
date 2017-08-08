da3575aaf25f81395f085151d778c8496546498c
gitgrimbo <gitgrimbo@gmail.com>
2017-08-08 15:39:12 +0100

    style: quotes

--- www/src/App.js
+++ www/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 const DAYS = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
 
