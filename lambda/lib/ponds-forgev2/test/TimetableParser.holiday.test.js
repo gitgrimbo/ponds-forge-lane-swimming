54b7cb5e7993a9d5aa1e1033fd3883cec4c338b6
gitgrimbo <gitgrimbo@gmail.com>
2018-08-08 11:26:09 +0100

    refactor: split timetable tests so we can see days in the console

--- lambda/lib/ponds-forgev2/test/TimetableParser.holiday.test.js
+++ lambda/lib/ponds-forgev2/test/TimetableParser.holiday.test.js
@@ -6,69 +6,66 @@ const { newTimetableItem, expectTimetableItem, newAlterations } = require("./uti
 const TimetableParser = require("../TimetableParser");
 
 describe("ponds-forgev2/TimetableParser", () => {
-    it("holiday-swimming-ponds-forge.html", () => {
-        const html = fs.readFileSync(path.join(__dirname, "./holiday-swimming-ponds-forge.html"));
+    const html = fs.readFileSync(path.join(__dirname, "./holiday-swimming-ponds-forge.html"));
 
-        const timetable = TimetableParser.timetableFromHTML(html, { trace: true });
+    const timetable = TimetableParser.timetableFromHTML(html, { trace: true });
 
+    it("expect 7 days", () => {
         expect(timetable).to.have.lengthOf(7);
+    });
 
-        function checkMonday() {
-            const tt0 = timetable[0];
-            expect(tt0).to.be.an("object");
-            expect(tt0.day).to.equal(1);
-            expect(tt0.items).to.have.lengthOf(3);
-            const expectedGeneralAlterations = newAlterations([
-                "Monday 30th July: Lane swimming sessions only available 6:30am - 9:45am in the 25 metre Leisure Pool. Swimfit Cancelled",
-                "Monday 27th August: Lane swimming available 8:00am - 8:00pm in the 25 metre Competition Pool",
-            ]);
-            expectTimetableItem(tt0.items[0], newTimetableItem("1", "06:30", "22:00", "Lane (25m) Swimming", "Competition Pool", expectedGeneralAlterations));
-            expectTimetableItem(tt0.items[1], newTimetableItem("1", "12:00", "14:00", "Age UK", "Competition Pool", expectedGeneralAlterations));
-            expectTimetableItem(tt0.items[2], newTimetableItem("1", "20:30", "21:30", "Swimfit", "Competition Pool", expectedGeneralAlterations));
-        }
+    it("monday", () => {
+        const tt0 = timetable[0];
+        expect(tt0).to.be.an("object");
+        expect(tt0.day).to.equal(1);
+        expect(tt0.items).to.have.lengthOf(3);
+        const expectedGeneralAlterations = newAlterations([
+            "Monday 30th July: Lane swimming sessions only available 6:30am - 9:45am in the 25 metre Leisure Pool. Swimfit Cancelled",
+            "Monday 27th August: Lane swimming available 8:00am - 8:00pm in the 25 metre Competition Pool",
+        ]);
+        expectTimetableItem(tt0.items[0], newTimetableItem("1", "06:30", "22:00", "Lane (25m) Swimming", "Competition Pool", expectedGeneralAlterations));
+        expectTimetableItem(tt0.items[1], newTimetableItem("1", "12:00", "14:00", "Age UK", "Competition Pool", expectedGeneralAlterations));
+        expectTimetableItem(tt0.items[2], newTimetableItem("1", "20:30", "21:30", "Swimfit", "Competition Pool", expectedGeneralAlterations));
+    });
 
-        function checkTuesday() {
-            const tt0 = timetable[1];
-            expect(tt0).to.be.an("object");
-            expect(tt0.day).to.equal(2);
-            expect(tt0.items).to.have.lengthOf(4);
-            expectTimetableItem(tt0.items[0], newTimetableItem("1", "06:30", "15:15", "Lane (25m) Swimming", "Competition Pool", newAlterations([
-                "Tuesday 31st July: Lane swimming sessions only available 6:30am - 9:45am in the 25 metre Leisure Pool",
-            ])));
-            // This next item is interesting as it's not formatted consistently in the HTML,
-            // hence the parser gives a strange description and doesn't even try with the room.
-            expectTimetableItem(tt0.items[2], newTimetableItem("1", "15:15", "16:00", "Lane Swimming", "Competition Pool", newAlterations([
-                "Tuesday 31st July: Lane swimming sessions only available 6:30am - 9:45am in the 25 metre Leisure Pool",
-            ])));
-        }
+    it("tuesday", () => {
+        const tt0 = timetable[1];
+        expect(tt0).to.be.an("object");
+        expect(tt0.day).to.equal(2);
+        expect(tt0.items).to.have.lengthOf(4);
+        expectTimetableItem(tt0.items[0], newTimetableItem("1", "06:30", "15:15", "Lane (25m) Swimming", "Competition Pool", newAlterations([
+            "Tuesday 31st July: Lane swimming sessions only available 6:30am - 9:45am in the 25 metre Leisure Pool",
+        ])));
+        // This next item is interesting as it's not formatted consistently in the HTML,
+        // hence the parser gives a strange description and doesn't even try with the room.
+        expectTimetableItem(tt0.items[2], newTimetableItem("1", "15:15", "16:00", "Lane Swimming", "Competition Pool", newAlterations([
+            "Tuesday 31st July: Lane swimming sessions only available 6:30am - 9:45am in the 25 metre Leisure Pool",
+        ])));
+    });
 
-        function checkSaturday() {
-            const tt = timetable[5];
-            expect(tt).to.be.an("object");
-            expect(tt.day).to.equal(6);
-            expect(tt.items).to.have.lengthOf(1);
-            expectTimetableItem(tt.items[0], newTimetableItem("1", "08:00", "20:00", "Lane (50m) Swimming", "Competition Pool", newAlterations([
-                "Saturday 28th July: Lane swimming unavailable all day",
-                "Saturday 4th August: Lane swimming unavailable all day",
-                "Saturday 11th August: Lane swimming available 8:00am - 8:00pm in the 50 metre Competition Pool",
-                "Saturday 18th August: Lane swimming available 8:00am - 8:00pm in the 25 metre Diving Pool",
-                "Saturday 25th August: Lane swimming available 8:00am - 7:00pm in the 25 metre Competition Pool and 7:00pm - 8:00pm in the 50 metre Competition Pool",
-            ])));
-        }
 
-        function checkSunday() {
-            const tt = timetable[6];
-            expect(tt).to.be.an("object");
-            expect(tt.day).to.equal(0);
-            expect(tt.items).to.have.lengthOf(1);
-            expectTimetableItem(tt.items[0], newTimetableItem("1", "08:00", "20:00", "Lane (50m) Swimming", "Competition Pool", newAlterations([
-                "Sunday 29th July: Lane swimming unavailable all day",
-            ])));
-        }
+    it("saturday", () => {
+        const tt = timetable[5];
+        expect(tt).to.be.an("object");
+        expect(tt.day).to.equal(6);
+        expect(tt.items).to.have.lengthOf(1);
+        expectTimetableItem(tt.items[0], newTimetableItem("1", "08:00", "20:00", "Lane (50m) Swimming", "Competition Pool", newAlterations([
+            "Saturday 28th July: Lane swimming unavailable all day",
+            "Saturday 4th August: Lane swimming unavailable all day",
+            "Saturday 11th August: Lane swimming available 8:00am - 8:00pm in the 50 metre Competition Pool",
+            "Saturday 18th August: Lane swimming available 8:00am - 8:00pm in the 25 metre Diving Pool",
+            "Saturday 25th August: Lane swimming available 8:00am - 7:00pm in the 25 metre Competition Pool and 7:00pm - 8:00pm in the 50 metre Competition Pool",
+        ])));
+    });
 
-        checkMonday();
-        checkTuesday();
-        checkSaturday();
-        checkSunday();
+
+    it("sunday", () => {
+        const tt = timetable[6];
+        expect(tt).to.be.an("object");
+        expect(tt.day).to.equal(0);
+        expect(tt.items).to.have.lengthOf(1);
+        expectTimetableItem(tt.items[0], newTimetableItem("1", "08:00", "20:00", "Lane (50m) Swimming", "Competition Pool", newAlterations([
+            "Sunday 29th July: Lane swimming unavailable all day",
+        ])));
     });
 });
--- lambda/lib/ponds-forgev2/test/TimetableParser.test.js
+++ lambda/lib/ponds-forgev2/test/TimetableParser.test.js
@@ -6,66 +6,61 @@ const { newTimetableItem, expectTimetableItem } = require("./utils");
 const TimetableParser = require("../TimetableParser");
 
 describe("ponds-forgev2/TimetableParser", () => {
-    it("swimming-ponds-forge.html", () => {
-        const html = fs.readFileSync(path.join(__dirname, "./swimming-ponds-forge.html"));
+    const html = fs.readFileSync(path.join(__dirname, "./swimming-ponds-forge.html"));
 
-        const timetable = TimetableParser.timetableFromHTML(html, { trace: true });
+    const timetable = TimetableParser.timetableFromHTML(html, { trace: true });
 
+    it("expect 7 days", () => {
         expect(timetable).to.have.lengthOf(7);
+    });
 
-        function checkMonday() {
-            const tt0 = timetable[0];
-            expect(tt0).to.be.an("object");
-            expect(tt0.day).to.equal(1);
-            expect(tt0.items).to.have.lengthOf(4);
-            expectTimetableItem(tt0.items[0], newTimetableItem("1", "06:30", "08:00", "Lane (25m) Swimming", "Diving Pool", []));
-            expectTimetableItem(tt0.items[1], newTimetableItem("1", "08:00", "22:00", "Lane (25m) Swimming", "Competition Pool", [{
-                message: "(Monday 23rd July: Lane swimming session only available 6:30am - 9:45am and 8:30pm - 10:00pm in the 25 metre Leisure Pool and Monday 30th July: Lane swimming sessions only available 6:30am - 9:45am in the 25 metre Leisure Pool)",
-            }]));
-        }
+    it("monday", () => {
+        const tt0 = timetable[0];
+        expect(tt0).to.be.an("object");
+        expect(tt0.day).to.equal(1);
+        expect(tt0.items).to.have.lengthOf(4);
+        expectTimetableItem(tt0.items[0], newTimetableItem("1", "06:30", "08:00", "Lane (25m) Swimming", "Diving Pool", []));
+        expectTimetableItem(tt0.items[1], newTimetableItem("1", "08:00", "22:00", "Lane (25m) Swimming", "Competition Pool", [{
+            message: "(Monday 23rd July: Lane swimming session only available 6:30am - 9:45am and 8:30pm - 10:00pm in the 25 metre Leisure Pool and Monday 30th July: Lane swimming sessions only available 6:30am - 9:45am in the 25 metre Leisure Pool)",
+        }]));
+    });
 
-        function checkTuesday() {
-            const tt0 = timetable[1];
-            expect(tt0).to.be.an("object");
-            expect(tt0.day).to.equal(2);
-            expect(tt0.items).to.have.lengthOf(4);
-            expectTimetableItem(tt0.items[0], newTimetableItem("1", "06:30", "15:15", "Lane (25m) Swimming", "Competition Pool", [{
-                message: "(Tuesday 24th July: Lane swimming session only available 6:30am - 9:45am in the 25 metre Leisure Pool and Tuesday 31st July: Lane swimming sessions only available 6:30am - 9:45am in the 25 metre Leisure Pool)",
-            }]));
-            // This next item is interesting as it's not formatted consistently in the HTML,
-            // hence the parser gives a strange description and doesn't even try with the room.
-            expectTimetableItem(tt0.items[2], newTimetableItem("1", "15:15", "16:00", "Lane Swimming", "Competition Pool", [{
-                message: "(Tuesday 24th July: Lane swimming session only available 8:30pm - 10.00pm in the 25 metre Leisure Pool and 31st July: Lane swimming session is cancelled)",
-            }]));
-        }
+    it("tuesday", () => {
+        const tt0 = timetable[1];
+        expect(tt0).to.be.an("object");
+        expect(tt0.day).to.equal(2);
+        expect(tt0.items).to.have.lengthOf(4);
+        expectTimetableItem(tt0.items[0], newTimetableItem("1", "06:30", "15:15", "Lane (25m) Swimming", "Competition Pool", [{
+            message: "(Tuesday 24th July: Lane swimming session only available 6:30am - 9:45am in the 25 metre Leisure Pool and Tuesday 31st July: Lane swimming sessions only available 6:30am - 9:45am in the 25 metre Leisure Pool)",
+        }]));
+        // This next item is interesting as it's not formatted consistently in the HTML,
+        // hence the parser gives a strange description and doesn't even try with the room.
+        expectTimetableItem(tt0.items[2], newTimetableItem("1", "15:15", "16:00", "Lane Swimming", "Competition Pool", [{
+            message: "(Tuesday 24th July: Lane swimming session only available 8:30pm - 10.00pm in the 25 metre Leisure Pool and 31st July: Lane swimming session is cancelled)",
+        }]));
+    });
 
-        function checkSaturday() {
-            const tt = timetable[5];
-            expect(tt).to.be.an("object");
-            expect(tt.day).to.equal(6);
-            expect(tt.items).to.have.lengthOf(1);
-            expectTimetableItem(tt.items[0], newTimetableItem("1", "08:00", "20:00", "Lane (50m) Swimming", "Competition Pool", [{
-                message: "Saturday 21st July: Lane swimming available 6:30pm - 8:00pm in the Diving Pit",
-            }, {
-                message: "Saturday 28th July: Lane swimming unavailable all day",
-            }]));
-        }
+    it("saturday", () => {
+        const tt = timetable[5];
+        expect(tt).to.be.an("object");
+        expect(tt.day).to.equal(6);
+        expect(tt.items).to.have.lengthOf(1);
+        expectTimetableItem(tt.items[0], newTimetableItem("1", "08:00", "20:00", "Lane (50m) Swimming", "Competition Pool", [{
+            message: "Saturday 21st July: Lane swimming available 6:30pm - 8:00pm in the Diving Pit",
+        }, {
+            message: "Saturday 28th July: Lane swimming unavailable all day",
+        }]));
+    });
 
-        function checkSunday() {
-            const tt = timetable[6];
-            expect(tt).to.be.an("object");
-            expect(tt.day).to.equal(0);
-            expect(tt.items).to.have.lengthOf(1);
-            expectTimetableItem(tt.items[0], newTimetableItem("1", "08:00", "20:00", "Lane (50m) Swimming", "Competition Pool", [{
-                message: "Sunday 22nd: Lane swimming unavailable all day",
-            }, {
-                message: "Sunday 29th July: Lane swimming unavailable all day",
-            }]));
-        }
-
-        checkMonday();
-        checkTuesday();
-        checkSaturday();
-        checkSunday();
+    it("sunday", () => {
+        const tt = timetable[6];
+        expect(tt).to.be.an("object");
+        expect(tt.day).to.equal(0);
+        expect(tt.items).to.have.lengthOf(1);
+        expectTimetableItem(tt.items[0], newTimetableItem("1", "08:00", "20:00", "Lane (50m) Swimming", "Competition Pool", [{
+            message: "Sunday 22nd: Lane swimming unavailable all day",
+        }, {
+            message: "Sunday 29th July: Lane swimming unavailable all day",
+        }]));
     });
 });
