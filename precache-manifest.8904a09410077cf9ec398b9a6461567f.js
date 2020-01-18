b2e0805848c88d67a5a602155f156d1cdab09bb5
gitgrimbo <gitgrimbo@gmail.com>
2020-01-18 13:15:42 +0000

    Updates

---
+++ precache-manifest.8904a09410077cf9ec398b9a6461567f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c697102766d911bf45279d15eb662f",
+    "url": "/ponds-forge-lane-swimming/index.html"
+  },
+  {
+    "revision": "6a8fb04d5070bb4d5dd9",
+    "url": "/ponds-forge-lane-swimming/static/css/main.9d9faa12.chunk.css"
+  },
+  {
+    "revision": "c3bf6a797e1a942c3a8e",
+    "url": "/ponds-forge-lane-swimming/static/js/2.a45785f0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ponds-forge-lane-swimming/static/js/2.a45785f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a8fb04d5070bb4d5dd9",
+    "url": "/ponds-forge-lane-swimming/static/js/main.5ef47a82.chunk.js"
+  },
+  {
+    "revision": "60ac654f888f3dc129db",
+    "url": "/ponds-forge-lane-swimming/static/js/runtime-main.45172f75.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ponds-forge-lane-swimming/precache-manifest.b63ea83b02b6cbe3bb0825a1b3a78bcc.js"
+  "/ponds-forge-lane-swimming/precache-manifest.8904a09410077cf9ec398b9a6461567f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ponds-forge-lane-swimming/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ponds-forge-lane-swimming/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
